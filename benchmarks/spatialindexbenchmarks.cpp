4b9d91c7b243eb8554a1ef053a250d97e783be49
Buck McCready <jbuckmccready@gmail.com>
2020-04-01 01:00:01 -0700

    Updated submodule and added spatial index stack reuse benchmark

--- benchmarks/spatialindexbenchmarks.cpp
+++ benchmarks/spatialindexbenchmarks.cpp
@@ -12,7 +12,6 @@ static void BM_createSpatialIndex(benchmark::State &state) {
     (void)_;
     cavc::createApproxSpatialIndex(profile.pline);
   }
-
 }
 BENCHMARK(BM_createSpatialIndex)->Unit(benchmark::kMicrosecond);
 
@@ -24,17 +23,35 @@ static void BM_querySpatialIndex(benchmark::State &state) {
   std::vector<std::size_t> queryResults;
   for (auto _ : state) {
     (void)_;
-    for (std::size_t i = 0; i < profile.pline.size(); ++i) {
-      cavc::AABB<double> bb;
-      bb.xMin = bb.xMax = profile.pline[i].x();
-      bb.yMin = bb.yMax = profile.pline[i].y();
+    profile.pline.visitSegIndices([&](std::size_t i, std::size_t j) {
+      cavc::AABB<double> bb = cavc::createFastApproxBoundingBox(profile.pline[i], profile.pline[j]);
       queryResults.clear();
-      const double delta = 5.0;
-      bb.expand(delta);
+      bb.expand(5);
       spatialIndex.query(bb.xMin, bb.yMin, bb.xMax, bb.yMax, queryResults);
-    }
+      return true;
+    });
   }
 }
 BENCHMARK(BM_querySpatialIndex)->Unit(benchmark::kMicrosecond);
+
+static void BM_querySpatialIndexReuseStack(benchmark::State &state) {
+  auto profile = circle();
+  profile.pline = cavc::convertArcsToLines(profile.pline, arcError);
+  state.counters["vertexCount"] = static_cast<double>(profile.pline.size());
+  auto spatialIndex = cavc::createApproxSpatialIndex(profile.pline);
+  std::vector<std::size_t> queryResults;
+  std::vector<std::size_t> queryStack;
+  for (auto _ : state) {
+    (void)_;
+    profile.pline.visitSegIndices([&](std::size_t i, std::size_t j) {
+      cavc::AABB<double> bb = cavc::createFastApproxBoundingBox(profile.pline[i], profile.pline[j]);
+      queryResults.clear();
+      bb.expand(5);
+      spatialIndex.query(bb.xMin, bb.yMin, bb.xMax, bb.yMax, queryResults, queryStack);
+      return true;
+    });
+  }
+}
+BENCHMARK(BM_querySpatialIndexReuseStack)->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
